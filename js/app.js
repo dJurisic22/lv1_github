1649c006852fd8adb9dc7a162974193af3c3af6a
dJurisic22 <domagoj.jurisic@vsmti.hr>
2021-03-31 11:05:28 +0200

    test

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+function Dodaj(){
+	var table = document.getElementById("tablica");
+	var input1=document.getElementById("nazivPredmeta").value;
+	var input2=document.getElementById("godina").value;
+	var input3=document.getElementById("ECTS").value;
+	
+	var row = table.insertRow(1);
+	
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	var cell3 = row.insertCell(2);
+	var cell4 = row.insertCell(3);
+	var cell5 = row.insertCell(4);
+
+	cell1.innerHTML=input1;
+	cell2.innerHTML=input2;
+	cell3.innerHTML=input3;
+	cell4.innerHTML="<button type='button' onclick='Obrisi(this)' id='obrisiButton'>Obrisi</button>";
+	cell5.innerHTML="<input type='checkbox' id='boja' onclick='promjeniBoju(this)'>";
+}
+
+function Obrisi(btn){
+	 var row = btn.parentNode.parentNode;
+  	row.parentNode.removeChild(row);
+}
+
 function promjeniBoju(btn){
 	if(document.getElementById('boja').checked){
 		var row = btn.parentNode.parentNode;
@@ -8,32 +34,6 @@ function promjeniBoju(btn){
   		row.style.backgroundColor='White';		
 	}
 	
-	
-}
-
-function Obrisi(btn){
-	 var row = btn.parentNode.parentNode;
-  	row.parentNode.removeChild(row);
-}
-
-function Dodaj(){
-	var table = document.getElementById("tablica");
-	var input1=document.getElementById("nazivPredmeta").value;
-	var input2=document.getElementById("godina").value;
-	var input3=document.getElementById("ECTS").value;
-	var row = table.insertRow(1);
-	
-	var cell1 = row.insertCell(0);
-	var cell2 = row.insertCell(1);
-	var cell3 = row.insertCell(2);
-	var cell4 = row.insertCell(3);
-	var cell5 = row.insertCell(4);
-
-cell1.innerHTML=input1;
-cell2.innerHTML=input2;
-cell3.innerHTML=input3;
-cell4.innerHTML="<button type='button' onclick='Obrisi(this)' id='obrisiButton'>Obrisi</button>";
-cell5.innerHTML="<input type='checkbox' id='boja' onclick='promjeniBoju(this)'>";
 }
 
 function Trazi(){
